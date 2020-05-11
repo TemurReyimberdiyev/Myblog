fd4712198e0a61cc3345d8498b7cecd71b7daf25
Temur_Reyimberdiyev <temur.rey98@gmail.com>
2020-05-11 23:28:19 +0500

    complete blog

---
+++ footer.php
@@ -0,0 +1,36 @@
+<!--footer-->
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 footer-grid-agileits-w3ls text-left">
+					<h3>About US</h3>
+					<p><?php getshortdescription("titles"); ?></p>
+					<div class="read">
+						<a href="about.php" class="btn btn-primary read-m">Read More</a>
+					</div>
+				</div>
+				</div>
+			</div> -->
+			<!-- footer -->
+			<div class="footer-cpy text-center">
+				<div class="footer-social">
+					<div class="copyrighttop">
+						<ul>
+							<li class="mx-3">
+								<a class="facebook" href="<?php getlinks("links","facebook");?>">
+									<i class="fab fa-facebook-f"></i>
+									<span>Facebook</span>
+								</a>
+							</li>
+							<li>
+								<a class="facebook" href="<?php getlinks("links","twitter");?>">
+									<i class="fab fa-twitter"></i>
+									<span>Twitter</span>
+								</a>
+							</li>
+						</ul>
+
+					</div>
+			<!-- //footer -->
+		</div>
+	</footer>
